0c3562f64c643897427d9248cafece5db4b733ce
gitithan <gitit.han@gmail.com>
2012-06-22 16:06:17 +0800

    修改.gitignore

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -2,4 +2,23 @@
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
 
+;; default TAB key is occupied by auto-complete
+(global-set-key (kbd "C-c ; u") 'yas/expand)
+
+
+;; default hotkey `C-c & C-s` is still valid
+(global-set-key (kbd "C-c ; s") 'yas/insert-snippet)
+
+
+;; give yas/dropdown-prompt in yas/prompt-functions a chance
+(require 'dropdown-list)
+
+
+;; use yas/completing-prompt when ONLY when `M-x yas/insert-snippet'
+;; thanks to capitaomorte for providing the trick.
+(defadvice yas/insert-snippet (around use-completing-prompt activate)
+     "Use `yas/completing-prompt' for `yas/prompt-functions' but only here..."
+       (let ((yas/prompt-functions '(yas/completing-prompt)))
+             ad-do-it))
+
 (provide 'init-yasnippet)
